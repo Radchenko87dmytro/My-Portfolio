2660455508584d7e605b7db33582307086e73f77
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-01-30 21:03:55 +0100

    created Todolist.tsx component

---
+++ src/Todolist.tsx
@@ -0,0 +1,37 @@
+function sum(a: number, b: number) {
+  alert(a + b);
+}
+
+//sum(12, 14);
+
+export function Todolist(props: any) {
+  // props = {title: "What to learn"}
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <div>
+        <input />
+        <button>+</button>
+      </div>
+      <ul>
+        <li>
+          <input type={"checkbox"} checked={true} />
+          <span>CSS&HTML</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={true} />
+          <span>JS</span>
+        </li>
+        <li>
+          <input type="checkbox" checked={false} />
+          <span>React</span>
+        </li>
+      </ul>
+      <div>
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </div>
+    </div>
+  );
+}
