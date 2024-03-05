d3240f49d36c2b45a0271e3b541abfcf95282759
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-03-05 12:18:54 +0100

    feature: manage input state

--- src/Editablespan.tsx
+++ src/Editablespan.tsx
@@ -1,6 +1,34 @@
+import { useState, ChangeEvent } from "react";
+
 type EditableSpanPropsType = {
   title: string;
 };
+
 export function EditableSpan(props: EditableSpanPropsType) {
-  return <span>{props.title}-</span>;
+  let [editMode, setEditMode] = useState(false);
+  let [title, setTitle] = useState("");
+
+  const activateEditMode = () => {
+    setEditMode(true);
+  };
+
+  const activateViewMode = () => {
+    setEditMode(false);
+    setTitle(props.title);
+  };
+
+  const onChangeTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.currentTarget.value);
+  };
+
+  return editMode ? (
+    <input
+      value={title}
+      onChange={onChangeTitleHandler}
+      onBlur={activateViewMode}
+      autoFocus
+    ></input>
+  ) : (
+    <span onDoubleClick={activateEditMode}>{props.title}-</span>
+  );
 }
