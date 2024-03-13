eefb4a8e701f7b064ea5fc5c9ef02625b284b8a5
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-03-13 21:18:05 +0100

    feature: created unitType ActionsType

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,6 +1,10 @@
+//import { TodolistType } from './../App';
 import { v1 } from "uuid";
-import { TodolistType } from "../App";
-import { todolistsReducer } from "./todolists-reducer";
+import { FilterValuesType, TodolistType } from "../App";
+import {
+  ChangeTodolistFilterActionType,
+  todolistsReducer,
+} from "./todolists-reducer";
 
 test("correct todolist should be removed", () => {
   let todolistId1 = v1();
@@ -32,11 +36,57 @@ test("correct todolist should be added", () => {
   ];
 
   const endState = todolistsReducer(startState, {
-    type: "Add-TODOLIST",
+    type: "ADD-TODOLIST",
     title: newTodolistTitle,
   });
 
   expect(endState.length).toBe(3);
   expect(endState[2].title).toBe(newTodolistTitle);
   expect(endState[2].filter).toBe("all");
+});
+
+test("correct todolist should change its name", () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  let newTodolistTitle = "New Todolist";
+
+  const startState: Array<TodolistType> = [
+    { id: todolistId1, title: "What to learn", filter: "all" },
+    { id: todolistId2, title: "What to buy", filter: "all" },
+  ];
+
+  const action = {
+    type: "CHANGE-TODOLIST-TITLE" as const,
+    id: todolistId2,
+    title: newTodolistTitle,
+  };
+
+  const endState = todolistsReducer(startState, action);
+
+  expect(endState[0].title).toBe("What to learn");
+  expect(endState[1].title).toBe(newTodolistTitle);
+});
+
+test("correct filter of todolist should be changed", () => {
+  let todolistId1 = v1();
+  let todolistId2 = v1();
+
+  let newFilter: FilterValuesType = "completed";
+
+  const startState: Array<TodolistType> = [
+    { id: todolistId1, title: "What to learn", filter: "all" },
+    { id: todolistId2, title: "What to buy", filter: "all" },
+  ];
+
+  const action = {
+    type: "CHANGE-TODOLIST-FILTER" as const,
+    id: todolistId2,
+    filter: newFilter,
+  };
+
+  const endState = todolistsReducer(startState, action);
+
+  expect(endState[0].filter).toBe("all");
+  expect(endState[1].filter).toBe(newFilter);
 });
--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,21 +1,41 @@
 import { title } from "process";
-import { TodolistType } from "../App";
+import { FilterValuesType, TodolistType } from "../App";
 import { v1 } from "uuid";
 
-type ActionType = {
-  type: string;
-  [key: string]: any;
+export type RemoveTodolistActionType = {
+  type: "REMOVE-TODOLIST";
+  id: string;
 };
+export type AddTodolistActionType = {
+  type: "ADD-TODOLIST";
+  title: string;
+};
+export type ChangeTodolistActionType = {
+  type: "CHANGE-TODOLIST-TITLE";
+  id: string;
+  title: string;
+};
+export type ChangeTodolistFilterActionType = {
+  type: "CHANGE-TODOLIST-FILTER";
+  id: string;
+  filter: FilterValuesType;
+};
+
+type ActionsType =
+  | RemoveTodolistActionType
+  | AddTodolistActionType
+  | ChangeTodolistActionType
+  | ChangeTodolistFilterActionType;
 
 export const todolistsReducer = (
   state: Array<TodolistType>,
-  action: ActionType
+  action: ActionsType
 ): Array<TodolistType> => {
   switch (action.type) {
     case "REMOVE-TODOLIST": {
       return state.filter((tl) => tl.id != action.id);
     }
-    case "Add-TODOLIST": {
+    case "ADD-TODOLIST": {
       return [
         ...state,
         {
@@ -24,6 +44,20 @@ export const todolistsReducer = (
           filter: "all",
         },
       ];
+    }
+    case "CHANGE-TODOLIST-TITLE": {
+      const todolist = state.find((tl) => tl.id === action.id);
+      if (todolist) {
+        todolist.title = action.title;
+      }
+      return [...state];
+    }
+    case "CHANGE-TODOLIST-FILTER": {
+      const todolist = state.find((tl) => tl.id === action.id);
+      if (todolist) {
+        todolist.filter = action.filter;
+      }
+      return [...state];
     }
     default:
       throw new Error("I don't understand this action type");
