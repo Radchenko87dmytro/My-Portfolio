13cbbbdad018acc74116ae6f5d206b5e28272c9d
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2024-07-12 22:43:07 +0200

    feature: created input validation functionality

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import { FilterValuesType } from "./App";
-import { ChangeEvent, useState } from "react";
+import {
+  ChangeEvent,
+  DetailedHTMLProps,
+  InputHTMLAttributes,
+  useEffect,
+  useState,
+} from "react";
 
 export interface TaskType {
   id: string;
   title: string;
+  isDone: boolean;
+}
+interface Task {
+  // isDone?: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> | undefined
   isDone: boolean;
 }
 
@@ -13,10 +23,30 @@ export interface PropsType {
   removeTask: (id: string) => void;
   changeFilter: (value: FilterValuesType) => void;
   addTask: (title: string) => void;
+  //changeTaskStatus: (id:string, isDone: boolean) => void
 }
 
 export function Todolist(props: PropsType) {
   const [newTaskTitle, setNewTaskTitle] = useState("");
+  const [error, setError] = useState("");
+  const [isDone, setIsDone] = useState<Task>({ isDone: false });
+  const [btnDisabled, setBtnDisabled] = useState(false);
+
+  // Set status
+  useEffect(() => {
+    if (newTaskTitle.trim() === "") {
+      setBtnDisabled(true);
+      setError("Type please the text");
+      //setNewTaskTitle("Type please the text");
+    } else if (newTaskTitle.trim().length >= 20) {
+      setBtnDisabled(true);
+      setError("Text schould be schorter");
+      // setNewTaskTitle("Text schould be schorter");
+    } else {
+      setBtnDisabled(false);
+      setError("");
+    }
+  }, [newTaskTitle]);
   console.log(newTaskTitle);
 
   const taskTitleHandler = (event: ChangeEvent<HTMLInputElement>): void => {
@@ -26,11 +56,24 @@ export function Todolist(props: PropsType) {
   const addTaskHandler = () => {
     props.addTask(newTaskTitle);
     setNewTaskTitle("");
+
+    // if (newTaskTitle.trim() !== "") {
+    //   props.addTask(newTaskTitle);
+    //   setNewTaskTitle("");
+    // setError("");
+    //}
   };
 
   const removeTaskHandler = (id: string) => {
     props.removeTask(id);
   };
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsDone({ isDone: event.target.checked });
+    console.log(isDone);
+  };
+
+  //const errorMessage = "Title is requered";
 
   return (
     <div className="todolist">
@@ -42,10 +85,16 @@ export function Todolist(props: PropsType) {
             value={newTaskTitle}
             onChange={taskTitleHandler}
           />
-          <button className="button" onClick={addTaskHandler}>
+          <button
+            className="button"
+            onClick={addTaskHandler}
+            disabled={btnDisabled}
+          >
             Add
           </button>
         </div>
+        {/* {errorMessage && !btnDisabled} */}
+        <p className="error">{error}</p>
         <ul>
           {props.tasks.map((t) => {
             return (
@@ -54,6 +103,8 @@ export function Todolist(props: PropsType) {
                   className="custom-checkbox"
                   type={"checkbox"}
                   checked={t.isDone}
+                  //checked={isDone}
+                  onChange={onChangeHandler}
                 />
                 <span>{t.title}</span>
                 <button
