8aa5947c179649e5bdaa42abae83821c4c2caa22
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2025-02-17 20:36:42 +0100

    Working on hidden Discouse burger menu

--- src/components/HeaderTailwind.tsx
+++ src/components/HeaderTailwind.tsx
@@ -43,6 +43,7 @@ const HeaderTailwind: React.FC = () => {
   //   { name: "Todolist", href: "/", current: "todolist" },
   //   // { name: "Next Project", href: "/next", current: undefined },
   // ];
+
   console.log(current);
 
   useEffect(() => {
@@ -90,24 +91,24 @@ const HeaderTailwind: React.FC = () => {
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            {/* <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
                 aria-hidden="true"
-                className="block h-6 w-6 group-data-[open]:hidden"
+                className="block size-6 group-data-open:hidden"
               />
               <XMarkIcon
                 aria-hidden="true"
-                className="hidden h-6 w-6 group-data-[open]:block"
+                className="hidden size-6 group-data-open:block"
               />
-            </DisclosureButton>
+            </DisclosureButton> */}
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
+            <div className="flex shrink-0 items-center">
               <img
                 alt="Your Company"
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
+                src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
                 className="h-8 w-auto"
               />
             </div>
@@ -251,7 +252,7 @@ const HeaderTailwind: React.FC = () => {
 
                   <MenuItem>
                     <Link
-                      to="/abz"
+                      to="/"
                       className="flex justify-center px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
                       onClick={userSignout}
                     >
